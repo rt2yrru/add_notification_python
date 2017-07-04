6bd8bd87e5f723f3761e25fc4fe99c53835bd3e4
siddht1 <dhar.75.ap@gmail.com>
2017-07-04 04:55:00 +0000

    Update notification.py
    some changes like showing level info,souce info,risk info

--- notification.py
+++ notification.py
@@ -1,8 +1,8 @@
 import os
 _notification=notification#a string which has all update info,obsolete info,etc.
-   #eg 1: update for 4.12 kernel available,
+   #eg 1: update for 4.12 kernel available from offical source,source:ubuntu,level :5,risk :high risk,
    # eg 2: this [package name] is a broken package
-   # eg 3: update you distro ,etc 
+   # eg 3: update you distro ,etc ,source:mint,level:1,risk:minimum
 os.system('notify-send '+_notification)   
 # and clear it afterwards
 os.system('clear')
