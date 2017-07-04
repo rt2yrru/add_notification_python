8a70ad2130c84d6f7a82b7f8ae526d02e6703e22
siddht1 <dhar.75.ap@gmail.com>
2017-07-04 05:05:20 +0000

    Update notification_subprocess.py
    fixed some errors

--- notification_subprocess.py
+++ notification_subprocess.py
@@ -1,7 +1,7 @@
 import subprocess
-def update:
-    _comamnd="sudo apt update"
+def _update():
+    _command="sudo apt update"
     get_info = subprocess.Popen(["/bin/bash", "-c", _command], stdout=subprocess.PIPE)
     return get_info
- _msg=str(update)   
-subprocess.Popen(["/bin/bash", "-c", _msg])
+_msg=str(_update())   
+subprocess.Popen(["/bin/bash", "-c", _msg]) 
